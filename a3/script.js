e591ec9c958bee2a55f530a110d09eb400a27e16
Hayden <155851970+HeyItsDen@users.noreply.github.com>
2024-02-07 17:26:29 -0800

    script.js
    script but fixeD????

--- a3/script.js
+++ a3/script.js
@@ -31,10 +31,10 @@ rLine +="</p>";
 
 }
 
-document.getElementById("upRight").innerHTML = rLine;
+document.getElementById("upLeft").innerHTML = rLine;
 }
 
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i > 0;i--){
 rLine +="<p>";
@@ -55,10 +55,10 @@ rLine +="</p>";
 
 }
 
-document.getElementById("downRight").innerHTML = rLine;
+document.getElementById("upRight").innerHTML = rLine;
 }
 
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=0;i<pHeight;i++){
 rLine +="<p>";
@@ -79,7 +79,7 @@ rLine +="</p>";
 
 }
 
-document.getElementById("upLeft").innerHTML = rLine;
+document.getElementById("downLeft").innerHTML = rLine;
 }
 
 function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -103,5 +103,25 @@ rLine +="</p>";
 
 }
 
-document.getElementById("downLeft").innerHTML = rLine;
+document.getElementById("downRight").innerHTML = rLine;
+}
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
 }
